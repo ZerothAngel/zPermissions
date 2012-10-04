6452712a8c28a7a40dd9d599ceac7f36e4a56752
ZerothAngel <zerothangel@tyrannyofheaven.org>
2012-10-03 22:07:06 -0700

    Impose an order to exported players and groups so import always works.

--- src/main/java/org/tyrannyofheaven/bukkit/zPermissions/ModelDumper.java
+++ src/main/java/org/tyrannyofheaven/bukkit/zPermissions/ModelDumper.java
@@ -18,8 +18,14 @@ package org.tyrannyofheaven.bukkit.zPermissions;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import org.tyrannyofheaven.bukkit.util.transaction.TransactionCallbackWithoutResult;
 import org.tyrannyofheaven.bukkit.zPermissions.model.Entry;
@@ -45,13 +51,13 @@ public class ModelDumper {
                             plugin.getDescription().getVersion(),
                             new Date()));
                     // Dump players first
-                    List<PermissionEntity> players = plugin.getDao().getEntities(false);
+                    List<PermissionEntity> players = sortPlayers(plugin.getDao().getEntities(false));
                     for (PermissionEntity entity : players) {
                         out.println(String.format("# Player %s", entity.getDisplayName()));
                         dumpPermissions(out, entity);
                     }
                     // Dump groups
-                    List<PermissionEntity> groups = plugin.getDao().getEntities(true);
+                    List<PermissionEntity> groups = sortGroups(plugin.getDao().getEntities(true));
                     for (PermissionEntity entity : groups) {
                         out.println(String.format("# Group %s", entity.getDisplayName()));
                         out.println(String.format("permissions group %s create", entity.getDisplayName()));
@@ -81,7 +87,7 @@ public class ModelDumper {
 
     // Dump permissions for a player or group
     private void dumpPermissions(final PrintWriter out, PermissionEntity entity) {
-        for (Entry e : entity.getPermissions()) {
+        for (Entry e : sortPermissions(entity.getPermissions())) {
             out.println(String.format("permissions %s %s set %s%s%s %s",
                     (entity.isGroup() ? "group" : "player"),
                     entity.getDisplayName(),
@@ -90,6 +96,83 @@ public class ModelDumper {
                     e.getPermission(),
                     e.isValue()));
         }
+    }
+
+    private List<PermissionEntity> sortPlayers(Collection<PermissionEntity> players) {
+        List<PermissionEntity> result = new ArrayList<PermissionEntity>(players);
+        // Just sort alphabetically
+        Collections.sort(result, new Comparator<PermissionEntity>() {
+            @Override
+            public int compare(PermissionEntity a, PermissionEntity b) {
+                return a.getDisplayName().compareTo(b.getDisplayName());
+            }
+        });
+        return result;
+    }
+
+    private List<PermissionEntity> sortGroups(Collection<PermissionEntity> groups) {
+        Queue<PermissionEntity> scanList = new LinkedList<PermissionEntity>();
+        
+        // Seed with parent-less groups
+        for (PermissionEntity group : groups) {
+            if (group.getParent() == null)
+                scanList.add(group);
+        }
+
+        List<PermissionEntity> result = new ArrayList<PermissionEntity>(groups.size());
+
+        // BFS from queue to get total ordering
+        while (!scanList.isEmpty()) {
+            PermissionEntity group = scanList.remove();
+            
+            // Add to result
+            result.add(group);
+            
+            // Grab children and add to end of scanList
+            List<PermissionEntity> children = new ArrayList<PermissionEntity>(group.getChildren().size());
+            children.addAll(group.getChildren());
+            
+            // Sort children alphabetically
+            Collections.sort(children, new Comparator<PermissionEntity>() {
+                @Override
+                public int compare(PermissionEntity a, PermissionEntity b) {
+                    return a.getDisplayName().compareTo(b.getDisplayName());
+                }
+            });
+
+            scanList.addAll(children);
+        }
+
+        return result;
+    }
+
+    private List<Entry> sortPermissions(Collection<Entry> entries) {
+        List<Entry> result = new ArrayList<Entry>(entries);
+        Collections.sort(result, new Comparator<Entry>() {
+            @Override
+            public int compare(Entry a, Entry b) {
+                if (a.getRegion() != null && b.getRegion() == null)
+                    return 1;
+                else if (a.getRegion() == null && b.getRegion() != null)
+                    return -1;
+                else if (a.getRegion() != null && b.getRegion() != null) {
+                    int regions = a.getRegion().getName().compareTo(b.getRegion().getName());
+                    if (regions != 0) return regions;
+                }
+
+                if (a.getWorld() != null && b.getWorld() == null)
+                    return 1;
+                else if (a.getWorld() == null && b.getWorld() != null)
+                    return -1;
+                else if (a.getWorld() != null && b.getWorld() != null) {
+                    int worlds = a.getWorld().getName().compareTo(b.getWorld().getName());
+                    if (worlds != 0) return worlds;
+                }
+
+                return a.getPermission().compareTo(b.getPermission());
+            }
+        });
+        return result;
     }
 
 }
