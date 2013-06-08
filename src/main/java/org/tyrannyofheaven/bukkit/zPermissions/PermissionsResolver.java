080ae0ed61b4d32e87394dfc6637e6cf56c618a4
ZerothAngel <zerothangel@tyrannyofheaven.org>
2013-06-08 10:19:45 -0700

    Backout commit: opaque-inheritance defaults to false now.

--- src/main/java/org/tyrannyofheaven/bukkit/zPermissions/PermissionsResolver.java
+++ src/main/java/org/tyrannyofheaven/bukkit/zPermissions/PermissionsResolver.java
@@ -48,7 +48,7 @@ public class PermissionsResolver {
 
     private String defaultGroup;
 
-    private boolean opaqueInheritance = false;
+    private boolean opaqueInheritance = true;
 
     private boolean interleavedPlayerPermissions = true;
 
