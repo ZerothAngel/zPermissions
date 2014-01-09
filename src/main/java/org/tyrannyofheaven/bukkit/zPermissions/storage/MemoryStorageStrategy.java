8808b2c62e57dac4cbcec564290f2b1ff7aee85d
ZerothAngel <zerothangel@tyrannyofheaven.org>
2014-01-08 20:56:30 -0800

    Add missing refresh feature to flat-file storage.

--- src/main/java/org/tyrannyofheaven/bukkit/zPermissions/storage/MemoryStorageStrategy.java
+++ src/main/java/org/tyrannyofheaven/bukkit/zPermissions/storage/MemoryStorageStrategy.java
@@ -20,8 +20,10 @@ import static org.tyrannyofheaven.bukkit.util.ToHLoggingUtils.log;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
@@ -39,7 +41,7 @@ import org.tyrannyofheaven.bukkit.zPermissions.dao.PermissionDao;
  */
 public class MemoryStorageStrategy implements StorageStrategy, TransactionStrategy, Runnable {
 
-    private static final int SAVE_DELAY = 200; // 10 seconds
+    private static final int SAVE_DELAY = 10; // seconds
 
     private final MemoryPermissionDao dao = new MemoryPermissionDao();
 
@@ -49,9 +51,9 @@ public class MemoryStorageStrategy implements StorageStrategy, TransactionStrate
 
     private boolean initialized;
 
-    private int saveTask = -1; // NB synchronized on this
+    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
-    private final Lock saveLock = new ReentrantLock();
+    private ScheduledFuture<?> saveTask; // protected by this
 
     public MemoryStorageStrategy(Plugin plugin, File saveFile) {
         this.plugin = plugin;
@@ -79,23 +81,52 @@ public class MemoryStorageStrategy implements StorageStrategy, TransactionStrate
         if (!initialized) return; // if didn't load properly DON'T overwrite
 
         // Kill scheduled async task
-        synchronized (this) {
-            if (saveTask > -1) {
-                // NB still race condition, but saveLock will serialize saves
-                Bukkit.getScheduler().cancelTask(saveTask);
-                saveTask = -1;
-            }
-        }
+        cancelSaveTask();
 
         debug(plugin, "Saving permissions database one last time...");
-        save();
+        // Queue up a final save task
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                save();
+            }
+        });
+        // Start shutting down
+        executorService.shutdown();
+        // And wait
+        try {
+            long timeout = 60L;
+            log(plugin, "Waiting up to %d seconds for pending write operations...", timeout);
+            if (!executorService.awaitTermination(timeout, TimeUnit.SECONDS)) {
+                log(plugin, Level.WARNING, "Timed out before all write operations could finish; expect inconsistencies :(");
+            }
+            else {
+                log(plugin, "All write operations done.");
+            }
+        }
+        catch (InterruptedException e) {
+            // Do nothing
+        }
     }
 
     @Override
-    public void refresh(boolean force, Runnable finishTask) {
-        // Do nothing
-        if (finishTask != null)
-            finishTask.run();
+    public void refresh(boolean force, final Runnable finishTask) {
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                cancelSaveTask();
+
+                try {
+                    dao.load(saveFile);
+                }
+                catch (IOException e) {
+                    log(plugin, Level.SEVERE, "Error loading permissions database:", e);
+                }
+                
+                if (finishTask != null)
+                    Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, finishTask);
+            }
+        });
     }
 
     @Override
@@ -127,10 +158,8 @@ public class MemoryStorageStrategy implements StorageStrategy, TransactionStrate
             // Schedule a save if dirty and no pending save
             if (dao.isDirty()) {
                 synchronized (this) {
-                    if (saveTask < 0) {
-                        saveTask = Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, this, SAVE_DELAY).getTaskId();
-                        if (saveTask < 0)
-                            log(plugin, Level.SEVERE, "Error scheduling permissions database save task");
+                    if (dao.isDirty() && saveTask == null) { // NB re-test dirty
+                        saveTask = executorService.schedule(this, SAVE_DELAY, TimeUnit.SECONDS);
                     }
                 }
             }
@@ -151,24 +180,29 @@ public class MemoryStorageStrategy implements StorageStrategy, TransactionStrate
     @Override
     public void run() {
         debug(plugin, "Auto-saving permissions database...");
-        synchronized (this) {
-            saveTask = -1;
-        }
         save();
+        synchronized (this) {
+            saveTask = null;
+        }
     }
 
+    // NB Only called from executorService, which is single-threaded
+    // All calls are therefore serialized
     private void save() {
         try {
-            saveLock.lock();
-            try {
-                dao.save(saveFile);
-            }
-            finally {
-                saveLock.unlock();
-            }
+            dao.save(saveFile);
         }
         catch (IOException e) {
             log(plugin, Level.SEVERE, "Error saving permissions database:", e);
+        }
+    }
+
+    private void cancelSaveTask() {
+        synchronized (this) {
+            if (saveTask != null) {
+                saveTask.cancel(false);
+            }
+            saveTask = null;
         }
     }
 
