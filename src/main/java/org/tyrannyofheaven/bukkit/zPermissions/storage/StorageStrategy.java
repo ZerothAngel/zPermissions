b8517a5d77475aa5764d509d4e75fbcad79a87fb
Allan Saddi <allan@saddi.com>
2014-01-18 13:53:11 -0800

    Javadocs for StorageStrategy.

--- src/main/java/org/tyrannyofheaven/bukkit/zPermissions/storage/StorageStrategy.java
+++ src/main/java/org/tyrannyofheaven/bukkit/zPermissions/storage/StorageStrategy.java
@@ -26,16 +26,56 @@ import org.tyrannyofheaven.bukkit.zPermissions.dao.PermissionDao;
  */
 public interface StorageStrategy {
 
+    /**
+     * Perform any required initialization. Called once from the zPermissions
+     * onEnable() handler.
+     */
     public void init();
     
+    /**
+     * Perform any required cleanup. Called from the zPermissions onDisable()
+     * handler.
+     */
     public void shutdown();
-    
+
+    /**
+     * A request to re-read from the permissions store. Whether this is actually
+     * done is implementation-dependent.
+     * 
+     * @param force A hint on whether or not to force a refresh. For conditional
+     *   refreshes, this will be <code>false</code>.
+     * @param finishTask If permissions were actually re-read and finishTask is
+     *   not-<code>null</code>, this task should be run <i>synchronously</i>
+     *   in the main thread.
+     */
     public void refresh(boolean force, Runnable finishTask);
 
+    /**
+     * Retrieve an instance of {@link PermissionDao} that is associated with
+     * this storage strategy.
+     * 
+     * @return the PermissionDao instance
+     */
     public PermissionDao getDao();
     
+    /**
+     * Retrieve an instance of {@link TransactionStrategy} that is associated
+     * with this storage strategy. This version is typically used for
+     * read-only transactions or transactions that are not safe to retry
+     * (because of being poorly written :P).
+     * 
+     * @return a TransactionStrategy
+     */
     public TransactionStrategy getTransactionStrategy();
-    
+
+    /**
+     * Retrieve an instance of {@link TransactionStrategy} that is associated
+     * with this storage strategy. This version should support retries if it
+     * makes sense (e.g. SQL transactions with rollback). Typical used with
+     * simple operations (single DAO method call).
+     * 
+     * @return a TransactionStrategy
+     */
     public TransactionStrategy getRetryingTransactionStrategy();
 
 }
